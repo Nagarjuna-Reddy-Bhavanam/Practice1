037e8a58723bdae74c2f41369d03759d0f692305
Nagarjuna-Reddy-Bhavanam <134576802+Nagarjuna-Reddy-Bhavanam@users.noreply.github.com>
2023-09-22 12:28:35 +0530

    Update Sum.java
    update

--- Demo/src/demo/org/Sum.java
+++ Demo/src/demo/org/Sum.java
@@ -4,7 +4,13 @@ public class Sum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+          int a,b,c,d;
+		a=10;
+		b=10;
+		c=10;
+		d=10;
+		sum =a+b+c+d;
+		System.out.println("sum = " + sum);
 	}
 
 }
