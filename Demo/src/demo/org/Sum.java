8dd9b3947e8af8757abe1937a699618e1be52798
Nagarjuna-Reddy-Bhavanam <134576802+Nagarjuna-Reddy-Bhavanam@users.noreply.github.com>
2023-09-22 12:34:18 +0530

    Update Sum.java
    modify

--- Demo/src/demo/org/Sum.java
+++ Demo/src/demo/org/Sum.java
@@ -5,10 +5,10 @@ public class Sum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           int a,b,c,d;
-		a=10;
-		b=10;
-		c=10;
-		d=10;
+		a=120;
+		b=120;
+		c=120;
+		d=120;
 		int sum =a+b+c+d;
 		System.out.println("sum = " + sum);
 	}
